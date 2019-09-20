c4966764addd2b9b3ead092c82a4a3923a634ed3
garrettameek <garrett.a.meek@gmail.com>
2019-09-19 19:19:49 -0600

    Minor changes to the model builder.
    Former-commit-id: 07c4aa8570444f685f4c862c0ab410a54e92428c
    Former-commit-id: 9925aa0d8cd885fec6ab254599812de5a99b7ce8
    Former-commit-id: a95ad365705382021189413d79157cd6c3185b63 [formerly 5fc1ede6d8b460b5a9bf5250f9b8590198418d79]
    Former-commit-id: 84602ae2df0884d79f4efe1033b90b47fb386055
    Former-commit-id: ca4c8cf9360782e4fbd7d986d3a96aaa576eb853 [formerly ecabd4ff4d86d5fc13e520aa0f0e61d2e995e03e]
    Former-commit-id: 971648344416ad8098cb63c45e4264715db31ab6

---
+++ build/lib/foldamers/ensembles/cluster.py
@@ -0,0 +1,90 @@
+from simtk import unit
+from msmbuilder.cluster.kcenters import KCenters
+import mdtraj as md
+from simtk.openmm.app.pdbfile import PDBFile
+from foldamers.utilities.iotools import write_pdbfile_without_topology
+
+def concatenate_trajectories(pdb_file_list,combined_pdb_file="combined.pdb"):
+        """
+        Given a list of PDB files, this function reads their coordinates, concatenates them, and saves the combined coordinates to a new file (useful for clustering with MSMBuilder).
+
+        :param pdb_file_list: A list of PDB files to read and concatenate
+        :type pdb_file_list: List( str )
+
+        :param combined_pdb_file: The name of file/path in which to save a combined version of the PDB files, default = "combined.pdb"
+        :type combined_pdb_file: str
+
+        :returns:
+          - combined_pdb_file ( str ) - The name/path for a file within which the concatenated coordinates will be written.
+
+        """
+        file = open(combined_pdb_file,mode="w")
+        for pdb_file in pdb_file_list:
+          pdb_obj = PDBFile(pdb_file)
+          frame=0
+          success = True
+          while success:
+            try:
+              positions = pdb_obj.getPositions(frame=frame)
+              topology = pdb_obj.getTopology()
+              PDBFile.writeFile(topology,positions,file=file)
+              frame = frame + 1
+            except:
+              success = False
+        file.close()
+        return(combined_pdb_file)
+
+def get_cluster_centroid_positions(pdb_file,cgmodel,n_clusters=None):
+        """
+        Given a PDB file and coarse grained model as input, this function performs K-means clustering on the poses in the PDB file, and returns a list of the coordinates for the "centroid" pose of each cluster.
+
+        :param pdb_file: The path/name of a file from which to read trajectory data
+        :type pdb_file: str
+
+        :param cgmodel: A CGModel() class object
+        :type cgmodel: class
+
+        :param n_clusters: The number of "bins" within which to cluster the poses from the input trajectory.
+        :type n_clusters: int
+
+        :returns:
+          - centroid_positions ( List ( np.array( float * unit.angstrom ( num_particles x 3 ) ) ) ) - A list of the poses corresponding to the centroids of all trajectory clusters.
+
+        """
+        centroid_positions = []
+        traj = md.load(pdb_file)
+        print(traj.n_frames)
+        exit()
+        if n_clusters == None:
+          n_clusters = 50
+        cluster_list = KCenters(n_clusters=n_clusters,metric='rmsd')
+        cluster_list.fit(traj)
+        centroid_list = cluster_list.cluster_centers_
+        centroid_index = 1
+        for centroid in centroid_list:
+          positions = centroid.xyz[0] * unit.nanometer
+          cgmodel.positions = positions
+          file_name = str("centroid_"+str(centroid_index)+".xyz")
+          write_pdbfile_without_topology(cgmodel,file_name)
+          centroid_positions.append(positions)
+        return(centroid_positions)
+
+def align_structures(reference_traj,target_traj):
+        """
+        Given a reference trajectory, this function performs a structural alignment for a second input trajectory, with respect to the reference.
+
+        :param reference_traj: The trajectory to use as a reference for alignment.
+        :type reference_traj: `MDTraj() trajectory <http://mdtraj.org/1.6.2/api/generated/mdtraj.Trajectory.html>`_
+
+        :param target_traj: The trajectory to align with the reference.
+        :type target_traj: `MDTraj() trajectory <http://mdtraj.org/1.6.2/api/generated/mdtraj.Trajectory.html>`_
+
+        :returns:
+          - aligned_target_traj ( `MDTraj() trajectory <http://mdtraj.org/1.6.2/api/generated/mdtraj.Trajectory.html>`_ ) - The coordinates for the aligned trajectory.
+
+        """
+
+        aligned_target_traj = target_traj.superpose(reference_traj)
+
+        return(aligned_target_traj)
+
